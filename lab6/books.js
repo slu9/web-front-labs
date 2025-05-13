392e82058dc9e185f1ecb4b47c51a6d3ceb48bbc
Софья Зырянова <s0niksluggard@yandex.ru>
2025-05-13 13:35:18 +0500

    Правки

---
+++ lab6/books.js
@@ -0,0 +1,12 @@
+const books = [
+    { author: "Достоевский Ф. М.", name: "Преступление и наказание", publicationYear: 1866, price: 500, bestseller: true, inStock: true },
+    { author: "Толстой Л. Н.", name: "Война и мир", publicationYear: 1869, price: 750, bestseller: true, inStock: false },
+    { author: "Булгаков М. А.", name: "Мастер и Маргарита", publicationYear: 1966, price: 620, bestseller: true, inStock: true },
+    { author: "Набоков В. В.", name: "Лолита", publicationYear: 1955, price: 540, bestseller: false, inStock: true },
+    { author: "Пушкин А. С.", name: "Евгений Онегин", publicationYear: 1833, price: 400, bestseller: false, inStock: true },
+    { author: "Оруэлл Дж.", name: "1984", publicationYear: 1949, price: 480, bestseller: true, inStock: false },
+    { author: "Брэдбери Р.", name: "451° по Фаренгейту", publicationYear: 1953, price: 390, bestseller: true, inStock: true },
+    { author: "Хемингуэй Э.", name: "Старик и море", publicationYear: 1952, price: 410, bestseller: false, inStock: true },
+    { author: "Сэлинджер Д.", name: "Над пропастью во ржи", publicationYear: 1951, price: 470, bestseller: true, inStock: true },
+    { author: "Чехов А. П.", name: "Вишнёвый сад", publicationYear: 1904, price: 360, bestseller: false, inStock: false }
+];
